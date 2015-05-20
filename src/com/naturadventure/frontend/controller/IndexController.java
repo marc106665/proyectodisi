0956b146a3cc3622869a7326cbae4f9402eebc6c
Pau Alegria <komikclasico@hotmail.com>
2015-05-20 19:20:03 +0200

    Termindad la función de actividad

--- src/com/naturadventure/frontend/controller/IndexController.java
+++ src/com/naturadventure/frontend/controller/IndexController.java
@@ -46,93 +46,69 @@ public class IndexController {
 	 @RequestMapping(value="/actividad/{tipo}", method = RequestMethod.GET) 
 	    public String viewTActividad(Model model, @PathVariable String tipo) {
 		 	model.addAttribute("tipoactividad", tipoActividadDao.getTipoActividad(tipo));
-		 //	HashMap<String, Dato> mapadeniveles = new HashMap<String, Dato>();
-		 
 		 	
 	        HashMap<String, List<Actividad>> mapadeniveles = new HashMap<String, List<Actividad>>();
-	       
-	       // HashMap<String, HashMap<Integer, Float>> idActividadPorNiveles = new HashMap<String, HashMap<Integer, Float>>();
+	       	HashMap<String, HashMap<Integer, Double>> mapaDePrecios = new HashMap<String, HashMap<Integer, Double>>();
+
 		 	List<NivelActividad> listaNiveles = new LinkedList<NivelActividad>(); 
 		 	List<Actividad> listaActividades = actividadDao.getActividadesDeTipo(tipo);
-	        List<String> listaNivelesUnicos = actividadDao.getNivelesUnicos();
+	      //  List<String> listaNivelesUnicos = actividadDao.getNivelesUnicos();
 	        
-	     //   List<Actividad> listaActividadesprintar = new LinkedList<Actividad>();
 	        String nivelVar = ""; 
 	        if (listaActividades != null){
 	        for(Actividad actividad : listaActividades) {
 	     
 	        	listaNiveles =  actividadDao.getNivelActividad(actividad.getIdActividad());
-	       	 	System.out.println("ide actividad  " + actividad.getIdActividad());
+	       	 	//System.out.println("ide actividad  " + actividad.getIdActividad());
 	       	 	
 	       	 	//Dato dato=null;
-	       	 	List<Actividad> auxlista = null;
+	       	 	
 	        	for(NivelActividad nivel : listaNiveles) {
-	        		
-	        		
+	        		List<Actividad> auxlista = new LinkedList<Actividad>();
+	        		HashMap<Integer, Double> auxmap = new HashMap<Integer, Double>();
+
 	        		HashMap<Integer, Double> precioPoridActividad = new HashMap<Integer, Double>();
 	        		precioPoridActividad.put(actividad.getIdActividad(), (double) nivel.getPrecioPorPersona());
 	        		
 	        		List<Actividad> listaActividadesVar = new LinkedList<Actividad>();
 	        	 	listaActividadesVar.add(actividad);
 	        	 	
-	        	 	
-	        	 	if(auxlista != null){ //Si ya existian valores...
-	        	 		auxlista = mapadeniveles.get(nivel.getNivel());
-	        			if(auxlista == null){
-	        				auxlista = new LinkedList<Actividad>();
-	        				auxlista.addAll(listaActividadesVar);
-	        			}else{
-	        				auxlista.addAll(listaActividadesVar);
-	        			}
-	        		}else// Es la primera iteracion...
-	        			auxlista = new LinkedList<Actividad>();
-	        	 		auxlista.addAll(listaActividadesVar);
-	        		
+	        	 	if ( mapadeniveles.get(nivel.getNivel()) != null ){
+	        	 		auxlista.addAll(mapadeniveles.get(nivel.getNivel()));	
+	        	 	}  
+	        	 	if ( mapaDePrecios.get(nivel.getNivel()) != null ){
+	        	 		auxmap.putAll(mapaDePrecios.get(nivel.getNivel()));	
+	        	 	}
+
+
+	        		auxlista.addAll(listaActividadesVar);
+	        		auxmap.putAll(precioPoridActividad);
+
 	        		mapadeniveles.put(nivel.getNivel(), auxlista);
+	        		mapaDePrecios.put(nivel.getNivel(), auxmap);
 	        		auxlista = null;
-	        	/*	if(dato != null){ //Si ya existian valores...
-	        			dato = mapadeniveles.get(nivel.getNivel());
-	        			if(dato == null){
-	        				dato = new Dato(listaActividadesVar, precioPoridActividad);
-	        				dato.añadir(listaActividadesVar, precioPoridActividad);
-	        			}else{
-	        				dato.añadir(listaActividadesVar, precioPoridActividad);
-	        			}
-	        		}else// Es la primera iteracion...
-	        			dato = new Dato(listaActividadesVar, precioPoridActividad); 
-	        		
-	        		mapadeniveles.put(nivel.getNivel(), dato);
-	        	*/
-	        	//	System.out.println(" nivel  " +nivel.getNivel() + " nombre actividad " + actividad.getIdActividad());
+	        		auxmap = null;
+	        		//System.out.println(" nivel  " +nivel.getNivel() + " nombre actividad " + actividad.getIdActividad());
 	        	}
 	       
 	        }
 	        }
-	        
-	    /*    for(String nivel2 : listaNivelesUnicos) {
-	        	System.out.println(" nivel  " +nivel2 );
-	        	List<Actividad> listacatividades = null;
-	        	listacatividades = mapadeniveles.get(nivel2);
-	        	for(Actividad act : listacatividades) {
-	        		System.out.println(" nivel  " +nivel2 + " nombre actividad " + act.getIdActividad());
-		        	
-		        }
-	        	
-	        }
-	      */  
+	      
 	       
-	       model.addAttribute("listaNiveles", listaNivelesUnicos );
+	    //   model.addAttribute("listaNiveles", listaNivelesUnicos );
 	       
 	       model.addAttribute("mapaListaActividadesPorNiveles", mapadeniveles );
+	       model.addAttribute("precioPoridActividad", mapaDePrecios );
+	       
 	       return "actividad"; 
 	       
-	       // model.addAttribute("mapaPreciosPorIdactividadesPorNiveles", idActividadPorNiveles );
+	    
 	        
 	       
 	    } 
 	 
-	 @RequestMapping("/actividad/reserva")
-	    public String reserva() {
+	 @RequestMapping(value="/actividad/reserva/{idActividad}", method = RequestMethod.GET)
+	    public String reserva(Model model, @PathVariable Integer idActividad) {
 		
 	        return "reserva";
 	    }

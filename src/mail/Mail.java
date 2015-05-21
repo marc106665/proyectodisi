def660c962a816b415254c00bbcf9571c81d26d4
al227781 <al227781@uji.es>
2015-05-21 19:24:58 +0200

    mail - falta hacer las plantillas para los mensajes

---
+++ src/mail/Mail.java
@@ -0,0 +1,157 @@
+package mail;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class Mail {
+
+	private final String USER_NAME = "*******";  // GMail user name (just the part before "@gmail.com")
+    private final String PASSWORD = "******"; // GMail password
+    private final String SUBJECT_ACCEPTED = "Confirmación";
+    private final String SUBJECT_REJECTED = "Reserva rechazada";
+    
+    private String from;
+    private String pwd;
+    private List<String> recipient = null;
+    private String subject="";
+    private String body = null;
+    
+    
+	public Mail(){
+		this.recipient = new LinkedList<String>();
+	};
+	
+	public Mail(String username, String password){
+		this.recipient = new LinkedList<String>();
+		this.from = username;
+		this.pwd = password;
+	}
+    public String getUsername(){
+    	return this.from;
+    }
+	
+    public String getPassword(){
+    	return this.pwd;
+    }
+    
+    public List<String> getRecipientList(){
+    	return this.recipient;
+    }
+    
+    public String getSubject(){
+    	return this.subject;
+    }
+    
+    public String getBody(){
+    	return this.body;
+    }
+    
+    
+    
+    public void setRecipient(String recipient){
+    	
+    	this.recipient.add(recipient);
+    }
+    
+    public void setUsername(String username){
+    	this.from = username;
+    }
+    
+    public void setPassword(String password){
+    	this.pwd = password;
+    }
+    
+    public void setUsernameAndPassword(String username, String password){
+    	this.from = username;
+    	this.pwd = password;
+    }
+    
+    public void setSubject(String subject){
+    	this.subject = subject;
+    }
+    
+    public void setDefaultAcceptSubject(){
+    	this.subject = SUBJECT_ACCEPTED;
+    }
+    
+    public void setDefaultRejectSubject(){
+    	this.subject = SUBJECT_REJECTED;
+    }
+    
+    public void setDefaultAcceptedBodyText(){
+    	String s = "";
+    	s=s+"Este es un mensaje auto-generado";
+    	s=s+"\n Su reserva ha sido aceptada";
+    	this.body = s;
+    }
+    
+    public void setDefaultRejectedBodyText(){
+    	String s = "";
+    	s=s+"Este es un mensaje auto-generado";
+    	s=s+"\n Su reserva ha sido rechaza por.....X motivos";
+    	this.body = s;
+    }
+    
+    public void setBodyText(String body){
+    	this.body = body;
+    }
+    	
+    
+    public void sendEmail(String from, String password, List<String> to, String subject, String body ){
+    	sendFromGMail(from, password, to, subject, body );
+    }
+    
+    
+    
+    private  void sendFromGMail(String from, String pass, List<String> to, String subject, String body) {
+        Properties props = System.getProperties();
+        String host = "smtp.gmail.com";
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.user", from);
+        props.put("mail.smtp.password", pass);
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+
+        Session session = Session.getDefaultInstance(props);
+        MimeMessage message = new MimeMessage(session);
+
+        try {
+            message.setFrom(new InternetAddress(from));
+            InternetAddress[] toAddress = new InternetAddress[to.size()-1];
+
+            // To get the array of addresses
+            for( int i = 0; i < to.size()-1; i++ ) {
+                toAddress[i] = new InternetAddress(to.get(i));
+            }
+
+            for( int i = 0; i < toAddress.length; i++) {
+                message.addRecipient(Message.RecipientType.TO, toAddress[i]);
+            }
+
+            message.setSubject(subject);
+            message.setText(body,"ISO-8859-1","html");
+            Transport transport = session.getTransport("smtp");
+            transport.connect(host, from, pass);
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+        }
+        catch (AddressException ae) {
+            ae.printStackTrace();
+        }
+        catch (MessagingException me) {
+            me.printStackTrace();
+        }
+    }
+	
+}
